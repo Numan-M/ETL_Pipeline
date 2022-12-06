944b4862b752018ea78f92a907da5529b3d6bc5d
aram-de <ms_aram@outlook.com>
2022-12-06 16:23:15 +0000

    Added tests and mock files for testing

--- extract.py
+++ extract.py
@@ -17,24 +17,20 @@ col_names = [
 file_path = "data/mockFile.csv"
 
 
-def turn_file_into_dataframe(file_path: str, col_names: list) -> pd.DataFrame:
+def turn_file_into_dataframe(file_path: str, col_names: list = col_names) -> pd.DataFrame :
+    try:
+        dataframe = pd.read_csv(Path(file_path), names=col_names)
+        dataframe.head()
+        return dataframe
+    except FileNotFoundError as e:
+        print(f"There was no file at {file_path}")
+        #print(e)
+        return e
+        
+    
 
-    df = pd.read_csv(Path(file_path), names=col_names)
-    df.head()
-    return df
-
-
-turn_file_into_dataframe(file_path, col_names)
-
-
-# happy path
-
-# Reads file with headers, returns a frame with headers
-
-
-# Reads file with no headers, returns a frame with headers
+print((turn_file_into_dataframe("data/mockFileWithMissingHeaders.csv", col_names)))
+print((type(turn_file_into_dataframe("data/mockFileWithMissingHeaders.csv", col_names))) == pd.DataFrame)
 
 
-# unhappy path
 
-# Reads file, file is empty
