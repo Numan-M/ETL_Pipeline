a7ed60e0e8324e0afd4102d24c5877be6afb0da8
Asif Brohi <asifbrohi@hotmail.co.uk>
2022-12-08 21:07:29 +0000

    Updated extract_cleanse file with loading and added a connection to db py file

---
+++ connect_to_db.py
@@ -0,0 +1,17 @@
+import psycopg2
+
+def connect_to_database(connection):
+    """
+    function to connect the the database, has 1 arguemnt connection which is a dict for the
+    parameters to connect to the database.
+    If successful it will return conn
+    """
+    try:
+        print("connecting")
+        conn = psycopg2.connect(**connection) 
+        print("done")
+    except:
+        print("not done")
+    return conn
+connection = {"database":"test", "user" : "postgres", "password" :"pass", "host" : "localhost", "port":"5432"}
+connecting_to_db = connect_to_database(connection)
--- extract_clean_load.py
+++ extract_clean_load.py
@@ -3,7 +3,9 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import pytest
-
+import psycopg2
+import psycopg2.extras as extras
+from connect_to_db import connecting_to_db
 col_names = [
     "timestamp",
     "store_name",
@@ -41,9 +43,54 @@ def remove_columns_from_df(
     """Removes specified columns from a dataframe"""
     try:
         dataframe.drop(columns_to_drop, axis=1, inplace=True)
+        dataframe['timestamp'] = pd.to_datetime(dataframe['timestamp'])
         return dataframe
     except:
         print("dataframe does not contain specified columns")
 
 
 clean_dataframe = remove_columns_from_df(turn_file_into_dataframe(file_path,col_names),to_drop)
+
+query =  ('''
+        CREATE TABLE IF NOT EXISTS orders(
+        order_id SERIAL PRIMARY KEY not null,
+        timestamp date not null,
+        store_name text not null,
+        products text not null,
+        total_price float not null,
+        payment_method text not null
+        
+        );
+        ''')
+
+def create_table(conn,query) ->psycopg2.extensions.connection:
+    cursor = conn.cursor()
+    cursor.execute(query)
+
+def insert_values_in_table(conn, df, table_used) -> psycopg2.extensions.cursor:
+    """
+    function below takes three arguemnts , connnection, dataframe and table name inside connection.
+    tuples--> creating a list of tuples from the dataframe. Has to be pass in values inside database
+    cols --> columns are comma-separated
+    query --> inserting
+    """
+
+    tuples = [tuple(x) for x in df.to_numpy()]
+
+
+    columns = ','.join(list(df.columns))
+    
+    query = "INSERT INTO %s(%s) VALUES %%s" % (table_used, columns)
+    cursor = conn.cursor()
+    try:
+        extras.execute_values(cursor, query, tuples)
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    
+    finally:
+        print("the dataframe is inserted")
+
+insert_values_in_table(connecting_to_db, clean_dataframe, 'orders')
+connecting_to_db.commit()
+connecting_to_db.close()
