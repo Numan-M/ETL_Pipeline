36270069d78a16bf33fb345cd6ff72a00925f968
aram-de <ms_aram@outlook.com>
2022-12-08 10:50:07 +0000

    Cleaned some mess I did and rolled back code to MVP

--- extract.py
+++ extract.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-import collections
 import numpy as np
 import pandas as pd
 import pytest
@@ -18,55 +17,19 @@ col_names = [
 file_path = "data/mockFile.csv"
 
 
-class HeadersWrongError(Exception):
-    pass
-
-
-def check_all_headers_present(
-    file_path: str, expected_col_names: list = col_names
-) -> bool:
-
-    """This function takes in a file through file_path and expected column headers and outputs
-    a Boolean True if all expected headers are present and are in the right order,
-    or False if there are missing headers"""
-
+def turn_file_into_dataframe(file_path: str, col_names: list = col_names) -> pd.DataFrame :
     try:
-        dataframe = pd.read_csv(Path(file_path))
-        actual_column_names = list(dataframe.columns)
-        return actual_column_names == expected_col_names
-
-    except FileNotFoundError as error:
+        dataframe = pd.read_csv(Path(file_path), names=col_names)
+        dataframe.head()
+        return dataframe
+    except FileNotFoundError as e:
         print(f"There was no file at {file_path}")
-        # print(e)
-        return error
+        #print(e)
+        return e
 
 
-def turn_file_into_dataframe(
-    file_path: str, col_names: list = col_names
-) -> pd.DataFrame:
 
-    """This function takes in a file through a file_path and column headers that default to
-    a value, in case the file has no headers and returns a dataframe with the headers provided
-    or the default headers"""
-
-    try:
-        if check_all_headers_present(file_path, col_names) or (len(col_names) != :
-            dataframe = pd.read_csv(Path(file_path), names=col_names)
-            return dataframe
-        else:
-            raise HeadersWrongError(
-                f"The file at {file_path} is has the wrong headers (some may be missing, have the wrong name, or be in the wrong order"
-            )
-
-    except FileNotFoundError as error:
-        print(f"There was no file at {file_path}")
-        # print(e)
-        return error
-
-
-#print((check_all_headers_present("data/mockFileWithMissingHeaders.csv", col_names)))
-# print((check_all_headers_present("data/mockFileWithHeaders.csv", col_names)))
-
-
-print((turn_file_into_dataframe("data/mockFile.csv", col_names)))
 # print((type(turn_file_into_dataframe("data/mockFileWithMissingHeaders.csv", col_names))) == pd.DataFrame)
+
+
+
