31eb622a42d497812155e93e827ea4ca528ff74a
aram-de <ms_aram@outlook.com>
2022-12-08 11:03:05 +0000

    Added doc string

--- extract.py
+++ extract.py
@@ -17,19 +17,19 @@ col_names = [
 file_path = "data/mockFile.csv"
 
 
-def turn_file_into_dataframe(file_path: str, col_names: list = col_names) -> pd.DataFrame :
+def turn_file_into_dataframe(
+    file_path: str, col_names: list = col_names
+) -> pd.DataFrame:
+
+    """Take a CSV file with the right number of columns and returns a data frame"""
+
     try:
         dataframe = pd.read_csv(Path(file_path), names=col_names)
-        dataframe.head()
         return dataframe
     except FileNotFoundError as e:
         print(f"There was no file at {file_path}")
-        #print(e)
+        # print(e)
         return e
 
 
-
 # print((type(turn_file_into_dataframe("data/mockFileWithMissingHeaders.csv", col_names))) == pd.DataFrame)
-
-
-
