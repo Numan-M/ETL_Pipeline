78c54321166ff8bedfec69cac367cca526061e34
Asif Brohi <asifbrohi@hotmail.co.uk>
2022-12-16 18:29:49 +0000

    implementing splitting of products column into ETL

--- extract_clean_load.py
+++ extract_clean_load.py
@@ -1,89 +1,118 @@
 from pathlib import Path
-
-import numpy as np
 import pandas as pd
-import pytest
 import psycopg2
 import psycopg2.extras as extras
+import numpy as np
 from connect_to_db import connecting_to_db
-col_names = [
-    "timestamp",
-    "store_name",
-    "customer_name",
-    "products",
-    "total_price",
-    "payment_method",
-    "card_number",
-]
-
-file_path = "data/mockFile.csv"
+from csv_into_df import column_names, to_drop_stores,to_drop_payment,to_drop_transaction
+from csv_into_df import df_products
+from creating_tables import script_store,script_payment,query_transaction,alter_t,query_products
+def main():
 
 
-def turn_file_into_dataframe(
-    file_path: str, col_names: list = col_names
-) -> pd.DataFrame:
+
+    file_path = "data/chesterfield_25-08-2021_09-00-00 - Copy.csv"
+
+
+    """Creating dataframes for each table"""
+    df = turn_file_into_dataframe(file_path,column_names)
+    df_payment= turn_file_into_dataframe(file_path,column_names)
+    df_transaction = turn_file_into_dataframe(file_path,column_names)
+    
+ 
+    """Cleaning & transforming dataframes"""
+    clean_df_store1 = remove_columns_from_df(df,to_drop_stores)
+    clean_df_store = clean_df_store1.drop_duplicates(inplace=False)
+    clean_df_payment1 = remove_columns_from_df(df_payment,to_drop_payment)
+    clean_df_payment = clean_df_payment1.drop_duplicates(inplace=False)
+    clean_df_transaction = remove_columns_from_df(df_transaction,to_drop_transaction)
+    clean_df_products = df_products
+    drop_p=["products","total_price","payment_method"]
+    clean_df_products.drop(drop_p, axis=1, inplace=True)
+    
+   
+    """Creating tables in DB"""
+    create_table(connecting_to_db,script_store)
+    create_table(connecting_to_db,script_payment)
+    create_table(connecting_to_db,query_transaction)
+    create_table(connecting_to_db,query_products)
+
+
+    """Loading data into DB"""
+    insert_values_in_table(connecting_to_db,clean_df_store,'stores')
+    insert_values_in_table(connecting_to_db,clean_df_payment,'payment_methods')
+    insert_values_in_table(connecting_to_db,clean_df_transaction,'transactions')
+    insert_values_in_table(connecting_to_db,clean_df_products,'products')
+
+    """altering tables in DB"""
+    alter_table(connecting_to_db,alter_t)
+    
+   
+
+
+def turn_file_into_dataframe(file_path, col_names) -> pd.DataFrame:
 
     """Take a CSV file with the right number of columns and returns a data frame"""
 
     try:
         dataframe = pd.read_csv(Path(file_path), names=col_names)
+        print("succesfully created Dataframe")
         return dataframe
+        
     except FileNotFoundError as e:
         print(f"There was no file at {file_path}")
         return e
 
 
-to_drop = ["customer_name", "card_number"]
-
-
-def remove_columns_from_df(
-    dataframe: pd.DataFrame, columns_to_drop: list
-) -> pd.DataFrame:
+def remove_columns_from_df(dataframe, columns_to_drop) -> pd.DataFrame:
 
     """Removes specified columns from a dataframe"""
     try:
-        dataframe.drop(columns_to_drop, axis=1, inplace=True)
         dataframe['timestamp'] = pd.to_datetime(dataframe['timestamp'])
+        dataframe.drop(columns_to_drop, axis=1, inplace=True)
+        
         return dataframe
     except:
         print("dataframe does not contain specified columns")
 
 
-clean_dataframe = remove_columns_from_df(turn_file_into_dataframe(file_path,col_names),to_drop)
+def create_table(conn,query):
+    try:
+        cursor = conn.cursor()
+    
+        cursor.execute(query)
+        print("table made")
+        conn.commit()
+    except:
+        print("not made")
 
-query =  ('''
-        CREATE TABLE IF NOT EXISTS orders(
-        order_id SERIAL PRIMARY KEY not null,
-        timestamp date not null,
-        store_name text not null,
-        products text not null,
-        total_price float not null,
-        payment_method text not null
-        
-        );
-        ''')
+def alter_table(conn,alter):
+    try:
+        cursor = conn.cursor()
+    
+        cursor.execute(alter)
+        print("table altered")
+        conn.commit()
+    except:
+        print("not altered")
 
-def create_table(conn,query) ->psycopg2.extensions.connection:
-    cursor = conn.cursor()
-    cursor.execute(query)
 
-def insert_values_in_table(conn, df, table_used) -> psycopg2.extensions.cursor:
-    """
-    function below takes three arguemnts , connnection, dataframe and table name inside connection.
-    tuples--> creating a list of tuples from the dataframe. Has to be pass in values inside database
-    cols --> columns are comma-separated
-    query --> inserting
-    """
 
+def insert_values_in_table(conn, df, table_used):
+    """Loading data into DB"""
+    
     tuples = [tuple(x) for x in df.to_numpy()]
+    
 
 
     columns = ','.join(list(df.columns))
     
     query = "INSERT INTO %s(%s) VALUES %%s" % (table_used, columns)
+    """--> INSERT INTO payment_methods(payment_method) VALUES ('CARD'),..."""
     cursor = conn.cursor()
     try:
         extras.execute_values(cursor, query, tuples)
+        
         conn.commit()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
@@ -91,6 +120,11 @@ def insert_values_in_table(conn, df, table_used) -> psycopg2.extensions.cursor:
     finally:
         print("the dataframe is inserted")
 
-insert_values_in_table(connecting_to_db, clean_dataframe, 'orders')
-connecting_to_db.commit()
-connecting_to_db.close()
+
+
+if __name__ == "__main__":
+    """
+    this runs the main function 
+    """
+    main() 
+
